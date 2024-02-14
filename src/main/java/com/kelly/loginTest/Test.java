6d509e29f4e1819ce5590337f97f5f3fb6d13ad6
kellyvasss <99kelvas@gafe.molndal.se>
2024-02-14 20:47:13 +0100

    added pom file info and test class for FE

---
+++ src/main/java/com/kelly/loginTest/Test.java
@@ -0,0 +1,16 @@
+package com.kelly.loginTest;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class Test extends VerticalLayout {
+
+
+    public Test() {
+        add(
+                new H1("HEJ")
+        );
+    }
+}
