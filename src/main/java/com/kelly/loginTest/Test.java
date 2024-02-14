82663d26094fdd384e3b7008834e9bc60f0ac2da
kellyvasss <99kelvas@gafe.molndal.se>
2024-02-14 21:28:11 +0100

    -

--- src/main/java/com/kelly/loginTest/Test.java
+++ src/main/java/com/kelly/loginTest/Test.java
@@ -4,7 +4,7 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("/")
+@Route(value = "")
 public class Test extends VerticalLayout {
 
 
